38e0d7e80723140ba65653e9735603fba8af0075
taryncodes@github.com <taryn.bilsborough@gmail.com>
2024-01-25 15:27:02 +0000

    Addition, subtraction, multiplication and division functions

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+# ADD
+def add(n1, n2):
+    return n1 + n2
+
+# subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# multiply
+def mutiply(n1, n2):
+    return n1 * n2
+
+# divide
+def devide(n1, n2):
+    return n1 / n2
+
