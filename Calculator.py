e5383cd3acf6e44afc40d0f9d24f8c70094f8658
taryncodes@github.com <taryn.bilsborough@gmail.com>
2024-01-25 15:47:35 +0000

    added calculation function to trigger the sum based on what operation symbol the user chose

--- Calculator.py
+++ Calculator.py
@@ -23,8 +23,11 @@ operations = {
     }
 
 num1 = int(input("Whats the first number?: "))
-num2 = int(input("whats your second number?: "))
-
 for symbols in operations:
     print(symbols)
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("whats your second number?: "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
 
+print(f"{num1} {operation_symbol} {num2} = {answer}")
