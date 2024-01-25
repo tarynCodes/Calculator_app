3cfd5d6dfabddc3ba884260c094ad0a20dc00ce6
taryncodes@github.com <taryn.bilsborough@gmail.com>
2024-01-25 15:32:37 +0000

    operations dictionary

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,23 @@
-# ADD
+
 def add(n1, n2):
     return n1 + n2
 
-# subtract
+
 def subtract(n1, n2):
     return n1 - n2
 
-# multiply
-def mutiply(n1, n2):
+
+def multiply(n1, n2):
     return n1 * n2
 
-# divide
-def devide(n1, n2):
+
+def divide(n1, n2):
     return n1 / n2
 
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+    }
